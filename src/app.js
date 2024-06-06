c961241055bec24271fa95f848af7230fe57b5d7
Justice Rivera <142175716+justicetrace@users.noreply.github.com>
2024-06-06 16:59:56 +0000

    name generator justicetrace

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const pronoun = ['the', 'our'];
+const adj = ['great', 'big'];
+const noun = ['jogger', 'racoon'];
+const extensions = ['.com', '.net'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const domain = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+            for (let l = 0; l < extensions.length; l++)
+                domain.push(`${pronoun[i]}${adj[j]}${noun[k]}${extensions[l]}`)
+        }
+    }
+}
+console.log(domain);
